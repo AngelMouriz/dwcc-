60ceb0201d5f9c58734cdc642bb7df175a1de5b5
AngelMouriz <a23angelmr@iessanclemente.net>
2023-11-12 20:22:18 +0100

    tarea2

---
+++ tarea2_ts.js
@@ -0,0 +1,83 @@
+var a = 1;
+do {
+    console.log("9 x " + a + " = " + 9 * a);
+    a++;
+} while (a <= 10);
+var b = 0;
+var c = 1;
+var d = 0;
+for (var a_1 = 0; a_1 <= 10; a_1++) {
+    console.log(b);
+    d = b + c;
+    b = c;
+    c = d;
+}
+var numero = parseInt(prompt("Comprobar si un numero entero es primo:"));
+if (numero <= 0) {
+    console.log("Error, el numero debe de ser un entero mayor que cero");
+}
+else {
+    var e = 2;
+    while (e <= numero) {
+        if (numero % e == 0 && numero != e) {
+            console.log("El numero no es primo!");
+            break;
+        }
+        if (numero == e) {
+            console.log("El numero es primo");
+        }
+        e++;
+    }
+}
+var f = parseInt(prompt("Introduce la edad (en numeros enteros)"));
+if (f >= 0 && f <= 12) {
+    console.log("Neno");
+}
+else if (f >= 13 && f <= 18) {
+    console.log("Adolescente");
+}
+else if (f >= 19 && f <= 30) {
+    console.log("Xoven");
+}
+else if (f >= 31 && f <= 64) {
+    console.log("Adulto");
+}
+else if (f >= 65 && f <= 100) {
+    console.log("Xubilado");
+}
+else {
+    console.log("Error na idade!!");
+}
+switch (true) {
+    case f >= 0 && f <= 12:
+        console.log("Neno");
+        break;
+    case f >= 13 && f <= 18:
+        console.log("Adolescente");
+        break;
+    case f >= 19 && f <= 30:
+        console.log("Xoven");
+        break;
+    case f >= 31 && f <= 64:
+        console.log("Adulto");
+        break;
+    case f >= 65 && f <= 100:
+        console.log("Xubilado");
+        break;
+    default:
+        console.log("Error na idade!!");
+        break;
+}
+var operando = 125;
+var resultado = 0;
+resultado = operando >> 3;
+console.log(resultado);
+operando = 40;
+resultado = operando << 2;
+console.log(resultado);
+operando = 25;
+resultado = operando >> 1;
+console.log(resultado);
+operando = 10;
+resultado = operando << 4;
+console.log(resultado);
---
+++ tarea2_ts.ts
@@ -0,0 +1,93 @@
+
+let a:number=1;
+do {
+    console.log("9 x "+a+" = "+ 9*a);
+    a++
+}
+while (a<=10);
+
+
+
+    let b:number=0;
+    let c:number=1;
+    let d:number=0;
+    for (let a=0; a<=10; a++) {
+        console.log(b);
+        d=b+c; 
+        b=c;
+        c=d;
+    }
+
+
+
+let numero:number = parseInt(prompt("Comprobar si un numero entero es primo:"));
+if (numero<=0){
+    console.log("Error, el numero debe de ser un entero mayor que cero");
+}
+else {
+let e:number=2;
+while (e<=numero) {
+
+    if (numero%e == 0 && numero != e) {
+        console.log("El numero no es primo!");
+        break;
+    }
+    if (numero==e) {
+        console.log("El numero es primo");
+    }
+    e++;
+}
+}
+
+let f:number = parseInt(prompt("Introduce la edad (en numeros enteros)"));
+if (f>=0 && f<=12) {
+    console.log("Neno");
+}
+else if (f>=13 && f<=18) {
+    console.log("Adolescente"); 
+}
+else if (f>=19 && f<=30) {
+    console.log("Xoven"); 
+}
+else if (f>=31 && f<=64) {
+    console.log("Adulto"); 
+}
+else if (f>=65 && f<=100) {
+    console.log("Xubilado"); 
+}
+else {
+    console.log("Error na idade!!")
+}
+switch (true) {
+    case f>=0 && f<=12:
+        console.log("Neno"); 
+        break;
+    case f>=13 && f<=18:
+        console.log("Adolescente"); 
+        break;
+    case f>=19 && f<=30:
+        console.log("Xoven"); 
+        break;
+    case f>=31 && f<=64:
+        console.log("Adulto"); 
+        break;  
+    case f>=65 && f<=100:
+        console.log("Xubilado"); 
+        break;  
+    default:
+     console.log("Error na idade!!")
+     break;
+}
+    let operando:number =125;
+    let resultado:number =0;
+    resultado = operando >> 3;
+    console.log(resultado);
+    operando=40;
+    resultado = operando << 2;
+    console.log(resultado);
+    operando=25;
+    resultado = operando >> 1;
+    console.log(resultado);
+    operando=10;
+    resultado = operando << 4;
+    console.log(resultado);
